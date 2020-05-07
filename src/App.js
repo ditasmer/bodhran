0091dcddb81db371c56f17282d7304d214ea3743
ditasmer <ditasmer@gmail.com>
2020-05-07 20:10:20 +0200

    Axios itunes albums

--- src/App.js
+++ src/App.js
@@ -1,46 +1,22 @@
 import React, { Component } from "react";
 import { render } from "@testing-library/react";
+import axios from "axios";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      isLoaded: false,
-    };
-  }
+  state = {
+    albums: [],
+    loading: false,
+  };
 
-  componentDidMount() {
-    fetch("https://itunes.apple.com/search?term=Queen&entity=album&limit=20")
-      .then((res) => res.json())
-      .then((json) => {
-        this.setState({
-          isLoaded: true,
-          items: json,
-        });
-      });
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://itunes.apple.com/search?term=Queen&entity=album&limit=20');
+
+    console.log(res.data);
   }
   render() {
-    var { isLoaded, items } = this.state;
-
-    if (!isLoaded) {
-      return <div> Loading...</div>;
-    } else {
-      return (
-        <div className="App">
-          {items.resultCount}
-          {
-            <ul>
-              {items.results.map((item, index) => (
-                <li key={index}>
-                   {index+1}. Album: {item.collectionName} - Artist: {item.artistName}
-                </li>
-              ))}
-            </ul>
-          }
-        </div>
-      );
-    }
+    return <div className="App">data:</div>;
   }
 }
 
