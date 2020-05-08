3f31181a6cd215de3ad8c398ff5dc0507da70a8c
ditasmer <ditasmer@gmail.com>
2020-05-08 12:57:20 +0200

    add title props

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class Navbar extends Component {
-    render() {
-        return (
-            <nav className='navbar bg-primary'>
-                <h1>Navbar</h1>
-            </nav>
-        )
-    }
+  static defaultProps = {
+    title: "Bodhran",
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+  };
+
+  render() {
+    return (
+      <nav className="navbar bg-primary">
+        <h1>{this.props.title}</h1>
+      </nav>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
